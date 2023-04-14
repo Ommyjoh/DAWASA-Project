313b8f8ce6b9179d903ca45508b2cfcc16d9b756
ommyjoh <emmanuelboshe@gmail.com>
2023-04-14 21:09:12 +0300

    adding background image in authentication page

--- resources/views/Layouts/customer/auth.blade.php
+++ resources/views/Layouts/customer/auth.blade.php
@@ -5,6 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>User Log in</title>
 
+  <style>
+    /* CSS styles go here */
+    body {
+        background-image: url('{{asset ('backend/dist/img/maji.jpg')}}');
+        background-size: cover;
+        background-position: center;
+      }
+    
+  </style>
+
   @include('layouts.partials.styles')
 </head>
 <body class="hold-transition login-page">
